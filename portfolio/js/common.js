d1b4318e9569d6add74129593c22c50d4b3d9af3
kimhyunwoo <diget.w@kakaocorp.com>
2018-03-18 22:42:37 +0900

    로딩함수 순서 변경

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -1,3 +1,38 @@
+imagesProgress();
+/* 로딩 스크립트 함수 */
+function imagesProgress() {
+    var $container = $('#progress'),
+        $progressBar = $container.find('.progress-bar'),
+        $progressText = $container.find('.progress-text'),
+        imgLoad = imagesLoaded('body'),
+        imgTotal = imgLoad.images.length,
+        imgLoaded = 0,
+        current = 0,
+        progressTimer = setInterval(updateProgress, 1000 / 60);
+    imgLoad.on('progress', function () {
+        imgLoaded++;
+    });
+    function updateProgress() {
+        var target = (imgLoaded / imgTotal) * 100;
+        current += (target - current) * 0.1;
+        $progressBar.css({width: current + '%'});
+        $progressText.text(Math.floor(current) + '%');
+        if (current >= 100) {
+            clearInterval(progressTimer);
+            $container.addClass('progress-complete');
+            $progressBar.add($progressText).delay(500).animate({opacity: 0}, 250, function () {
+                $container.animate({top: '-100%'}, 600);
+            });
+
+        };
+        if (current > 99.9) {
+            current = 100;
+            wheelEvent();
+            $('.tit_copy').addClass('active');
+        };
+    };
+};
+
 function viewUpdate() {
     $.ajax({
         url: "js/data.json",
@@ -190,38 +225,3 @@ setInterval (function () {
         t = 0;
     };
 },70);
-
-imagesProgress();
-/* 로딩 스크립트 함수 */
-function imagesProgress() {
-    var $container = $('#progress'),
-        $progressBar = $container.find('.progress-bar'),
-        $progressText = $container.find('.progress-text'),
-        imgLoad = imagesLoaded('body'),
-        imgTotal = imgLoad.images.length,
-        imgLoaded = 0,
-        current = 0,
-        progressTimer = setInterval(updateProgress, 1000 / 60);
-    imgLoad.on('progress', function () {
-        imgLoaded++;
-    });
-    function updateProgress() {
-        var target = (imgLoaded / imgTotal) * 100;
-        current += (target - current) * 0.1;
-        $progressBar.css({width: current + '%'});
-        $progressText.text(Math.floor(current) + '%');
-        if (current >= 100) {
-            clearInterval(progressTimer);
-            $container.addClass('progress-complete');
-            $progressBar.add($progressText).delay(500).animate({opacity: 0}, 250, function () {
-                $container.animate({top: '-100%'}, 600);
-            });
-
-        };
-        if (current > 99.9) {
-            current = 100;
-            wheelEvent();
-            $('.tit_copy').addClass('active');
-        };
-    };
-};
